aee3f658ac2acd366150763521891c188b2d5a30
Roeck <rons9n@gmail.com>
2021-12-05 17:33:19 +0000

    Game attributes variables

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+const gamesURL = "http://localhost:3000/games"
+const gameForm = document.getElementById("game-form")
+const gameName = document.getElementById("game-name")
+const gameProducer = document.getElementById("game-producer")
+const gameScore = document.getElementById("game-score")
+const gameImage = document.getElementById("game-image")
+const gameList = document.getElementById("card")
