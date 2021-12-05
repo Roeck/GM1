c8a2c4fc19148ecd8add035fdf9e18df0ddd8bf5
Roeck <rons9n@gmail.com>
2021-12-05 17:36:28 +0000

    submitGame function

--- client/src/game.js
+++ client/src/game.js
@@ -3,13 +3,37 @@ class Game {
     static allGames=[]
 
     constructor(game) {
-        this.id=game.id
-        this.name=game.attributes.name
-        this.producer=game.attributes.producer
-        this.score=game.attributes.score
-        this.image=game.attributes.image
-        this.comments=game.attributes.comments
+        this.id = game.id
+        this.name = game.attributes.name
+        this.producer = game.attributes.producer
+        this.score = game.attributes.score
+        this.image = game.attributes.image
+        this.comments = game.attributes.comments
 
         Game.allGames.push(this)
+    }
+
+    static submitGame(s) {
+        s.preventDefault()
+        fetch(gamesURL,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: gameName.value,
+                producer: gameProducer.value,
+                score: gameScore.value,
+                image: gameImage.value,
+            })
+        })
+            .then(response => response.json())
+            .then(game => {
+                let newGame = new Game(game.data)
+                // console.log(newGame)
+                newGame.renderGame()
+                gameForm.reset()
+            })
     }
 }
