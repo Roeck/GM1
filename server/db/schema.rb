cc79480ffdca23560aa4761a2a9aa4ddeaf5ec17
Roeck <rons9n@gmail.com>
2021-12-05 17:01:03 +0000

    Comment scaffold and migration

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_165026) do
+ActiveRecord::Schema.define(version: 2021_12_05_165839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.string "message"
+    t.bigint "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_comments_on_game_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_12_05_165026) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "games"
 end
