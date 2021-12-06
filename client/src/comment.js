fb86b292ebfee5bf6ef1bd658beb95c6c5c51517
Roeck <rons9n@gmail.com>
2021-12-06 00:13:37 +0000

    Comment constructor

---
+++ client/src/comment.js
@@ -0,0 +1,9 @@
+class Comment {
+    constructor(comment) {
+
+        this.id=comment.id
+        this.message=comment.message
+        this.game_id=comment.game_id
+        // console.log(comment.id)
+    }
+}
