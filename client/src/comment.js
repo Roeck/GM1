706977b95200a01b5904b9de736c5b699ecf7233
Roeck <rons9n@gmail.com>
2021-12-07 08:38:36 +0000

    Comment class contructor

---
+++ client/src/comment.js
@@ -0,0 +1,10 @@
+class Comment {
+
+    constructor(comment) {
+
+        this.id = comment.id
+        this.message = comment.message
+        this.game_id = comment.game_id
+        // console.log(comment.id)
+    }
+}
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,5 +1,5 @@
 Game.destroy_all
-# Comment.destroy_all
+Comment.destroy_all
 
 Game.create(name: "Alan Wake Remastered", producer: "Remedy", score: 9.0, image: "https://cdn1.epicgames.com/salesEvent/salesEvent/en_Remedy_DEER_S2_1200x1600_1200x1600-43dd4a2f28f9edbf0ed1461339fc11d4")
 
