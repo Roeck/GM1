1759e85ca66409509abc9f159d26c113d37d061f
Roeck <rons9n@gmail.com>
2021-12-07 08:45:35 +0000

    createComment function

--- client/src/comment.js
+++ client/src/comment.js
@@ -7,4 +7,14 @@ class Comment {
         this.game_id = comment.game_id
         // console.log(comment.id)
     }
+
+    static createComment(e) {
+        e.preventDefault()
+        const commentMessage = e.target.children[0].value
+        const commentList = e.target.previousElementSibling
+        const gameId = e.target.parentElement.dataset.id
+
+        // Comment.submitComment(commentMessage, commentList, gameId)
+        // e.target.reset()
+    }
 }
