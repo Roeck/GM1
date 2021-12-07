685ea5bd7dd1c882c7c4a48a26e3837bbe46534e
Roeck <rons9n@gmail.com>
2021-12-07 08:35:04 +0000

    Seeds file

--- client/src/game.js
+++ client/src/game.js
@@ -3,14 +3,57 @@ class Game {
     static allGames=[]
 
     constructor(game) {
-        this.id = game.id
-        this.name = game.attributes.name
-        this.producer = game.attributes.producer
-        this.score = game.attributes.score
-        this.image = game.attributes.image
-        this.comments = game.attributes.comments
+        this.id=game.id
+        this.name=game.attributes.name
+        this.producer=game.attributes.producer
+        this.score=game.attributes.score
+        this.image=game.attributes.image
+        this.comments=game.attributes.comments
 
         Game.allGames.push(this)
+    }
+
+    static renderGames() {
+        for(let game of this.allGames) {
+            game.renderGame()
+        }
+    }
+
+    static fetchGames() {
+        fetch(gamesURL)
+            .then(response => response.json())
+            .then(games => {
+                for(let game of games.data) {
+                    let newGame=new Game(game)
+                }
+                this.renderGames()
+            })
+    }
+
+    renderGame() {
+
+        const gameLi=document.createElement('card')
+        gameLi.dataset.id=this.id
+        gameList.appendChild(gameLi)
+
+        const h3=document.createElement('h3')
+        h3.className=("h3")
+        h3.innerText=this.name
+
+        const p=document.createElement('h6')
+        p.className=("card-text")
+        p.innerText="Producer: "+this.producer+" | "+"Score: "+this.score
+
+        const img=document.createElement('img')
+        img.src=this.image
+        img.width=130
+
+        const deleteButton=document.createElement('button')
+        deleteButton.className="btn btn-outline-secondary"
+        deleteButton.innerText="Remove"
+        deleteButton.addEventListener("click",this.deleteGame)
+
+        gameLi.append(h3,img,p,deleteButton)
     }
 
     static submitGame(s) {
@@ -30,54 +73,11 @@ class Game {
         })
             .then(response => response.json())
             .then(game => {
-                let newGame = new Game(game.data)
+                let newGame=new Game(game.data)
                 // console.log(newGame)
                 newGame.renderGame()
                 gameForm.reset()
             })
-    }
-
-    static fetchGames() {
-        fetch(gamesURL)
-            .then(response => response.json())
-            .then(games => {
-                for(let game of games.data) {
-                    let newGame = new Game(game)
-                }
-                this.renderGames()
-            })
-    }
-
-    static renderGames() {
-        for(let game of this.allGames) {
-            game.renderGames()
-        }
-    }   
-
-    renderGame() {
-
-        const gameLi = document.createElement('card')
-        gameLi.dataset.id = this.id
-        gameList.appendChild(gameLi)
-
-        const h3 = document.createElement('h3')
-        h3.className = ("h3")
-        h3.innerText = this.name
-
-        const p = document.createElement('h6')
-        p.className = ("card-text")
-        p.innerText = "Producer: " + this.producer + " | " + "Score: " + this.score
-
-        const img = document.createElement('img')
-        img.src = this.image
-        img.width = 130
-        
-        const deleteButton = document.createElement('button')
-        deleteButton.className = "btn btn-outline-secondary"
-        deleteButton.innerText = "Remove"
-        deleteButton.addEventListener("click",this.deleteGame)
-
-        gameLi.append(h3, img, p, deleteButton)
     }
 
     deleteGame() {
--- client/src/index.js
+++ client/src/index.js
@@ -5,3 +5,6 @@ const gameProducer = document.getElementById("game-producer")
 const gameScore = document.getElementById("game-score")
 const gameImage = document.getElementById("game-image")
 const gameList = document.getElementById("card")
+
+Game.fetchGames()
+gameForm.addEventListener("submit",Game.submitGame)
